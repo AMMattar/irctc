dd369d220f01c7b3e73ecee2fe2b61192d2a64b1
Ahmed Mattar <amattar@mylerz.com>
2023-11-04 01:08:28 +0200

    header

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+  logo: string = '../../../assets/images/logo.svg';
+
+}
